2c8870efd2614bc654ebf60858916a6c37d96cdf
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-17 00:04:54 -0700

    welcome test fix

--- test/features/can_access_welcome_test.rb
+++ test/features/can_access_welcome_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class CanAccessWelcomeTest < Capybara::Rails::TestCase
-  test "welcome message" do
+  def test test_welcome_page_message
     visit root_path
-    assert_content page, "Welcome"
-    refute_content page, "Goobye All!"
+    access page.has_content? "Welcome"
+    # assert_content page, "Welcome"
   end
 end
