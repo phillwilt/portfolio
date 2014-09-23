68383838ab81e2b8bfc05c2236fadad7cf1023cd
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-23 10:27:52 -0700

    create projects tests

--- test/features/creating_a_project_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-feature 'Creating a project' do
-  scenario 'submit a new project'  do
-
-  end
-end
--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -1,9 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
-class Projects::CreatingAProjectTest < Capybara::Rails::TestCase
-  test "sanity" do
-    visit root_path
-    assert_content page, "Hello World"
-    refute_content page, "Goobye All!"
+feature 'Creating a project' do
+
+  scenario 'index the projects' do
+
+  end
+
+  scenario 'create a new project'  do
+    visit projects_path
+
+    fill_in 'Name',  with: projects(:codefellows).name
+    fill_in 'Technologies', with: projects(:codefellows).technologies
+
+    click_on 'Create Project'
+
+    page.text.must_include 'Project was successfully created'
+    page.text.must_include projects(:codefellows).name
+  end
+
+  scenario 'edit a project' do
+  end
+
+  scenario 'delete a project' do
   end
 end
