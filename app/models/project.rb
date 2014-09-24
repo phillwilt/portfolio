1b4cf69b36c0e4b5ff3aee3c27cbf4578d6cab41
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-23 17:29:59 -0700

    unhappy path create test

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  validates :name, presence: true, length: { maximum: 60 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 60 }
   validates :technologies, presence: true, length: { maximum: 255 }
 end
--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -15,8 +15,21 @@ feature 'Creating a project' do
 
     click_on 'Create Project'
 
-    page.text.must_include "#{projects(:codefellows).name} was successfully created"
+    page.text.must_include "#{projects(:codefellows).name} was" \
+                            ' successfully created'
     page.text.must_include projects(:codefellows).name
+  end
+
+  scenario 'new project has invalid data' do
+    visit new_project_path
+
+    fill_in 'Name',  with: projects(:invalid).name
+    fill_in 'Technologies', with: projects(:invalid).technologies
+
+    click_on 'Create Project'
+
+    page.text.must_include 'too short'
+    page.text.must_include "can't be blank"
   end
 
   scenario 'edit a project' do
